ea336616b6e507280ca0eb1e6b0f336dd501c3a3
Riki Widiantoro <77282337+Riki-Toro@users.noreply.github.com>
2021-02-14 20:51:53 +0700

    Create selection-script2.js

---
+++ selection-script2.js
@@ -0,0 +1,17 @@
+// DOM SELECTION 2
+
+// 4. document.querySelector() >> element
+const p4 = document.querySelector('#b p');
+p4.style.color = 'green';
+p4.style.fontSize = '30px';
+
+const li2 = document.querySelector('#b ul li:nth-child(2)');
+li2.style.backgroundColor = 'orange';
+
+
+
+// 5. document.querySelectorAll() >> nodeList
+const p = document.querySelectorAll('p');
+for (let i = 0; i < p.length; i++) {
+	p[i].style.backgroundColor = 'red';
+}
