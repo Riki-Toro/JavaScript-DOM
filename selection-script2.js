20d93cdbeba81d097001c4f222cd950ce7856977
Riki Widiantoro <77282337+Riki-Toro@users.noreply.github.com>
2021-02-14 21:03:41 +0700

    Update selection-script2.js

--- selection-script2.js
+++ selection-script2.js
@@ -15,3 +15,8 @@ const p = document.querySelectorAll('p');
 for (let i = 0; i < p.length; i++) {
 	p[i].style.backgroundColor = 'red';
 }
+
+// Root
+const sectionB = document.querySelector('section#b');
+const p4 = sectionB.getElementsByTagName('p');
+p4[0].style.backgroundColor = 'red';
