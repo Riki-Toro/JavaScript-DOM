7f10216c03c68d0f2d0f360669044876f7c94b92
Riki Widiantoro <77282337+Riki-Toro@users.noreply.github.com>
2021-02-24 20:04:50 +0700

    Create object-intro.js

---
+++ object-intro.js
@@ -0,0 +1,34 @@
+// INTRO OBJECT
+
+// buat object
+let mhs = {
+	nama : 'Riki Widiantoro',
+	umur : 22,
+	ipSemester : [1,2,3,4],
+	alamat : {
+		jalan : 'abc',
+		kota : 'ponorogo',
+		provinsi : 'jatim'
+	}
+};
+
+
+
+// coba buat object
+let organisasi = {
+	nama : "Riki Widiantoro",
+	umur : 22,
+	divisi : "Publikasi",
+	NIA : 1234567890,
+	alamat : {
+		dukuh : "pondok",
+		desa : "belang",
+		kecamatan : "bungkal",
+		kabupaten : "ponorogo",
+		provinsi : "jawa timur",
+		kodePos : 63462,
+	},
+	prodi : "Teknik Informatika",
+	fakultas : "Teknik",
+	universitas : "Universitas Muhammadiyah Ponorogo"
+};
